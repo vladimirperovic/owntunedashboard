afa084ce55f6c9d084038897d5336129c5690939
Vladimir PEROVIC <vladimir.perovic@gmail.com>
2026-09-02 16:09:07 +0200

    Bump mobile dock alignment build

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ window.OWNTONE_DASHBOARD = {
  * that were not inserted by the HTML parser, so setting it only misleads.)
  */
 (() => {
-  const BUILD = '20260902-23';
+  const BUILD = '20260902-24';
   const asset = path => `${path}?v=${BUILD}`;
 
   const addStyle = href => {
